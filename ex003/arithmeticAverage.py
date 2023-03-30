893cf45993b46f7ea4f533463975295577be2500
unknown <davi.cesarbt@gmail.com>
2023-03-30 16:44:06 -0300

    third exercise made, student grades arithmetic average

---
+++ ex003/arithmeticAverage.py
@@ -0,0 +1,8 @@
+studentGrades = [8, 9, 9.5, 10, 10]
+sumStudentGrades = sum(studentGrades)
+lenthStudentGrades = len(studentGrades)
+
+studentGradesArithmeticAverage = sumStudentGrades / lenthStudentGrades
+print(studentGradesArithmeticAverage)
+
+# output : 9.3
