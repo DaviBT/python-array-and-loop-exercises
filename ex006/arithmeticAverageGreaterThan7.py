e6c92b0d2e40d3a478f0245f592058d013c114a5
unknown <davi.cesarbt@gmail.com>
2023-03-31 14:39:47 -0300

    exercise 006 beign made

---
+++ ex006/arithmeticAverageGreaterThan7.py
@@ -0,0 +1,27 @@
+student1 = []
+student2 = []
+student3 = []
+student4 = []
+student5 = []
+student6 = []
+student7 = []
+student8 = []
+student9 = []
+student10 = []
+
+grades = []
+gradesGreaterThan7 = [ ]
+a = 0
+
+while a < 4:
+    student1Input = input("Please, enter 4 student's grades: ")
+    studentGrades1 = int(student1Input)
+    studentGrades1Average
+    student1.append(studentGrades1)
+    a += 1
+    if studentGrades1 >= 7:
+        gradesGreaterThan7.append("student 01")
+
+
+print(f"The 10 student's grades are: {grades}")
+print(f"The 10 student's grades are: {gradesGreaterThan7}")
