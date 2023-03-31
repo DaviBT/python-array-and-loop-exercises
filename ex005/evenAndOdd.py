3acea63ad22288a7b1a25e8d4cf1cbd7f171a874
unknown <davi.cesarbt@gmail.com>
2023-03-31 09:02:05 -0300

    fifth exercise made, get the odd and even numbers from an array

--- ex005/evenAndOdd.py
+++ ex005/evenAndOdd.py
@@ -1,3 +1,12 @@
 numbers = [75, 11, 68, 97, 36, 56, 87, 1, 84, 55, 42, 20, 6, 76, 40, 64, 23, 32, 29, 83]
-oddNumbers
-evenNumbers
+
+evenNumbers = [ ] ## par
+oddNumbers = [ ] ## ímpar
+
+for num in numbers:
+    if num % 2  == 0:
+        evenNumbers.append(num)
+    else:
+        oddNumbers.append(num)
+
+print(f'The even numbers are {evenNumbers} and the odd numbers are are {oddNumbers}.')
