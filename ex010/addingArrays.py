6c1c2594756fa0bc72bb9e36cf568789cf9fd38c
unknown <davi.cesarbt@gmail.com>
2023-04-06 15:26:17 -0300

    ex010 made

---
+++ ex010/addingArrays.py
@@ -0,0 +1,7 @@
+array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+array2 = [" apple", "banana", "orange", "pear", "kiwi", "grape", "pineapple", "watermelon", "mango", "papaya"]
+
+array1Str = str(array1)
+arrayJunction = array1 + array2
+print(arrayJunction)
