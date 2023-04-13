0c80327aa2fec9232ada60d95583583c933a4a62
unknown <davi.cesarbt@gmail.com>
2023-04-13 10:27:42 -0300

    exercise 002 mas

---
+++ ex002/userAndPassword.py
@@ -0,0 +1,7 @@
+while True:
+   userInput = input("Enter your name: ")
+   passwordInput = input("Enter your password: ")
+   if userInput == passwordInput:
+        print("Your username and password can't be the same!")
+   else:
+    print(f"Your username is {userInput} and your password is {passwordInput}")
