446ef21c8e9751edd1d20a9291a51b8d0978b9d8
unknown <davi.cesarbt@gmail.com>
2023-04-13 11:30:52 -0300

    ex 004 made

---
+++ ex004/countryPopulation.py
@@ -0,0 +1,14 @@
+countryA = 80000 ## 80.000
+growthA = 1.03
+
+countryB = 200000 ## 200.000 
+growthB = 1.015
+
+years = 0
+
+while countryA < countryB:
+    countryA = countryA * growthA
+    countryB = countryB * growthB
+    years += 1
+
+print(f"It will be need {years} to country A overtake country B.")
