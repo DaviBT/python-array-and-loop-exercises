1f3a104e94ff11e04e9fa285c28a04ca030bd036
unknown <davi.cesarbt@gmail.com>
2023-04-13 16:32:13 -0300

    ex010 being made

---
+++ ex010/numbersInterval.py
@@ -0,0 +1,29 @@
+p = 0
+while p < 1:
+    numberInput = input("Enter a number: ")
+    numberInput = int(numberInput)
+    if numberInput < 1:
+        print("Enter a number above 1!")
+    else:
+        print(f"The number {numberInput} is valid!")
+        p = 1
+
+g = 0
+while g < 1:
+    finalNumberInput = input("Enter another number: ")
+    finalNumberInput = int(finalNumberInput)
+    if finalNumberInput < 1:
+        print("Enter a number above one.")
+    elif finalNumberInput > numberInput:
+        numberInput, finalNumberInput = finalNumberInput, numberInput
+        g = 1
+    else:
+        print(f"The number {finalNumberInput} is valid!")
+        g = 1
+
+intervalNumbers = list(range(numberInput, finalNumberInput))
+nums = []
+for i in intervalNumbers:
+    nums.append(i)
+
+print(nums)
